fa962114816f2aba38e5c97d6bace1e4fb0c79ef
asterno <amanturtoktonazarov@gmail.com>
2022-10-03 14:56:36 +0300

    Homeworkd

--- JS/4.2/script.js
+++ JS/4.2/script.js
@@ -10,12 +10,11 @@
 
 function fib() {
     let x = +prompt("Число Фибооначчи")
-    let a = 1
-    let b = 1
+
     for (let i = 0; i < x; i++) {
       let c = a + b
-      a = i - 1
-      b = i - 2
+   let   a = i - 1
+    let  b = i - 2
    console.log(c)
     }
 
