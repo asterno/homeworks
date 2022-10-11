a25c3eba0a9006ede93f830103ded623e8d259aa
asterno <amanturtoktonazarov@gmail.com>
2022-10-11 15:22:17 +0300

    homrworkd

--- JS/6.2/script.js
+++ JS/6.2/script.js
@@ -1,20 +1,18 @@
-let block = document.querySelector('div')
-let button = document.querySelector('button')
 
 
-function on(){
+     let block = document.querySelector('div')
+ let button = document.querySelector('button')
+
+    function on(){
     block.style.backgroundColor = 'green'
+    function off(){
+        block.style.backgroundColor = 'black'
+   
+    }
+    button.addEventListener('click',off)
+    
 }
 
-function off(){
-    block.style.backgroundColor = 'black'
-}
- if( block.style.backgroundColor =='green') {
-    button.addEventListener('click',off)
-}
- 
-if(block.style.backgroundColor == 'black'){
-    button.addEventListener('click',on)
- 
-}
- 
+
+
+button.addEventListener('click',on)
